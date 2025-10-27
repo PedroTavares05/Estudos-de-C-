86d12b9f9c2f3f4d272fe08da65548ba5fff801b
Pedro Henrique Tavares <phtsantos05@gmail.com>
2025-10-26 22:35:01 -0300

    correção do codigo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int A = Convert.ToInt32("5");
+int B = 5 + A;
+
+bool Condicao = Convert.ToBoolean(0);
+
+decimal Valor = Convert.ToDecimal("100,85");
+
+float Altura = float.Parse("1.80");
+
+Console.WriteLine(A);
+Console.WriteLine(B);
+Console.WriteLine(Altura.ToString("F2"));
+
+
+Console.WriteLine(Valor.ToString("C"));
+
+Console.WriteLine(Condicao);
